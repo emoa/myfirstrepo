1e38cb49db52c3af27b03241193c829665289d17
Jesús Antonio <moa@openmailbox.org>
2017-09-01 01:20:30 -0500

    api spc in script.js

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
 'use strict';
 
+let spc = {};
+
+Object.defineProperty(spc, 'namespace', {
+  value: function(names) {
+    let parts = names.split('\.'),
+    current = spc;
+    for (let i = 0; i < parts.length; i++) {
+      if (!current[parts[i]])
+      	current[parts[i]] = {};
+      current = current[parts[i]];
+    }
+  }
+});
+
+spc.namespace('DOM.table');
+console.log(JSON.stringify(spc));
+
 let ajax = {
   getXHR() {
-      let req;
-      if (window.XMLHttpRequest) {
-        req = new XMLHttpRequest();
-      } else {
-        req = new ActiveXObject('Microsoft.XMLHTTP');
-      }
-      return req;
-    },
-    request(reqType, url, cb, postBody) {
-      let req = this.getXHR();
-      req.addEventListener('load', cb, false);
-      req.open(reqType.toUpperCase(), url, true);
-      req.send(postBody || '');
+    let req;
+    if (window.XMLHttpRequest) {
+      req = new XMLHttpRequest();
+    } else {
+      req = new ActiveXObject('Microsoft.XMLHTTP');
     }
+    return req;
+  },
+  request(reqType, url, cb, postBody) {
+    let req = this.getXHR();
+    req.addEventListener('load', cb, false);
+    req.open(reqType.toUpperCase(), url, true);
+    req.send(postBody || '');
+  }
 }
 
 function mycb(evt) {
